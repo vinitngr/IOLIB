25aad202d10d96dd52cf15e38a77bca1c1e4b766
Aman Jangid <amanjangid7847@gmail.com>
2025-03-06 22:31:05 +0530

    Revert "Aman"

--- client/src/components/ui/testPage.tsx
+++
@@ -1,9 +0,0 @@
-export default function TestPage() {
-    return (
-        
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-3xl font-bold">Test Page</h1>
-    </div>
-    );
-
-}
